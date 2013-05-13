165f09cd5e5e3ce2b9a374d4c735dc5bebeb2f1d
Adam Doherty <adam.doherty@cfsgeeks.com>
2013-05-13 16:11:01 -0500

    Added logic for different types based on the office

--- vcc.py
+++ vcc.py
@@ -5,9 +5,19 @@ from lxml import etree,objectify
 app = Bottle()
 auth = ('admin','TANDBERG')
 headers = {'Content-type':'text/xml'}
+offices = {
+'kenora':{'ip':'69.26.70.18','type':'mxp'},
+'kenora2':{'ip':'69.26.70.21','type':'mxp'},
+'dryden':{'ip':'70.76.112.226', 'type':'c'},
+'siouxlookout':{'ip':'70.76.146.59','type':'c'},
+'redlake':{'ip':'76.66.221.36','type':'mxp'},
+'fortfrances':{'ip':'209','type':'c'}
+}
 
-def sendXML(ip,tmpl):
+def sendXML(office,tmpl):
+    tmpl = tmpl + '_%s' % offices[office]['type']
     data = template(tmpl)
+    ip = offices[office]['ip']
     r = requests.post('http://%s/putxml' % ip, headers=headers, auth=auth, data=data)
     return r.text
 
@@ -15,19 +25,19 @@ def sendXML(ip,tmpl):
 def get_favicon():
     return static_file('favicon.ico', root='./static/')
 
-@app.route('/<ip>')
-def index(ip):
+@app.route('/<office>')
+def index(office):
     req = requests.get('http://%s/getxml?location=/Status' % ip, auth=auth)
     xml = objectify.fromstring(req.content)
     response.content_type = 'text/plain'
     return xml.SystemUnit.Software.Version.text
 
-@app.route('/connect/<ip>')
-def connect(ip):
-    return sendXML(ip,'connect')
+@app.route('/connect/<office>')
+def connect(office):
+    return sendXML(office,'connect')
 
-@app.route('/hangup/<ip>')
-def hangup(ip):
-    return sendXML(ip, 'disconnectall')
+@app.route('/hangup/<office>')
+def hangup(office):
+    return sendXML(office, 'disconnectall')
 
 run(app,host='0.0.0.0',port='9900',reloader=True)
